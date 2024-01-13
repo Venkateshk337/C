4b47aa08a0d08e0e7f4cf963bf7f71014c9b19c3
venkateshk337 <146421288+venkateshk337@users.noreply.github.com>
2024-01-14 01:10:29 +0530

    Update pascal_triangle.c

--- class/pascal_triangle.c
+++ class/pascal_triangle.c
@@ -5,13 +5,13 @@ static int pascal[N][N];
 int i=0,j=0;
 int n=5;
 for(i;i<n;i++){
-    printf("\n\n");
+    printf("\n");
     for(j=0;j<=i;j++){
         if(i==j||j==0||i==n-1)
         pascal[i][j]=1;
         else{
         pascal[i][j]=pascal[i-1][j-1]+pascal[i-1][j];}
-        printf("%d\t",pascal[i][j]);
+        printf("%d ",pascal[i][j]);
     }
 }
 }
