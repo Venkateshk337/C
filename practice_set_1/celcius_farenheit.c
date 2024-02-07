51e47d65423031c3c4e100c85e71afaeca7e6dac
Goast <146421288+Venkateshk337@users.noreply.github.com>
2024-02-07 21:44:02 +0530

    Delete practice_set_1/celcius_farenheit.c

--- practice_set_1/celcius_farenheit.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-printf("Enter Celcius : ");
-float celcius;
-scanf("%f",&celcius);
-float Farenheit=(celcius*9/5)+32;
-printf("%f Celcius=%.4f Farenheit",celcius,Farenheit);
-    return 0;
-}
