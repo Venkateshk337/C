c1989a6af5cc8313a01125cce3ee8a7efa617056
Goast <146421288+Venkateshk337@users.noreply.github.com>
2024-02-07 21:50:49 +0530

    Delete practice_set_6/2_practice.c

--- practice_set_6/2_practice.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a=10;
-    printf("a value=%d\n",a);
-    printf("Address of a=%d\n",&a);
-    int *p=&a;
-    printf("*p=%d\n",*p);
-    printf("&p=%d\n",&p);
-}
